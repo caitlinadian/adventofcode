38ef67614cbedd289bee6cfaebb2f7287794e9e7
Caitlin Murphy <caitlin.murphy@surveygizmo.com>
2017-12-11 16:54:40 -0700

    fixing formatting

--- day 1/day1solutions.js
+++ day 1/day1solutions.js
@@ -2,19 +2,19 @@
 function solveCaptcha(input) {
 	var sum = 0;
 	for (i=0; i < input.length; i++) {
-    i == input.length - 1 && input[i] == input[0] ? sum += parseInt(input[i]) :
-    input[i] == input[i + 1] ? sum += parseInt(input[i]) :
-    sum += 0;
-  }
-  return sum;
+		i == input.length - 1 && input[i] == input[0] ? sum += parseInt(input[i]) :
+		input[i] == input[i + 1] ? sum += parseInt(input[i]) :
+		sum += 0;
+	}
+	return sum;
 }
 
 //part 2
 function solveCaptcha2(input) {
 	var sum = 0;
 	for (i=0; i < input.length; i++) {
-    input[i] == input[(i + input.length / 2) % input.length] ? sum += parseInt(input[i]) :
-    sum += 0;
-  }
-  return sum;
+		input[i] == input[(i + input.length / 2) % input.length] ? sum += parseInt(input[i]) :
+		sum += 0;
+	}
+	return sum;
 }
