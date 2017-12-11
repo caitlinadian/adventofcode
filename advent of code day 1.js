3c5e95e4159d15bc4b09238737ae509fa201840c
Caitlin Murphy <caitlin.murphy@surveygizmo.com>
2017-12-11 16:41:13 -0700

    adding day 1 part 2

--- advent of code day 1.js
+++ advent of code day 1.js
@@ -1,8 +1,19 @@
+//part 1
 function solveCaptcha(input) {
 	var sum = 0;
 	for (i=0; i < input.length; i++) {
     i == input.length - 1 && input[i] == input[0] ? sum += parseInt(input[i]) :
     input[i] == input[i + 1] ? sum += parseInt(input[i]) :
+    sum += 0;
+  }
+  return sum;
+}
+
+//part 2
+function solveCaptcha2(input) {
+	var sum = 0;
+	for (i=0; i < input.length; i++) {
+    input[i] == input[(i + input.length / 2) % input.length] ? sum += parseInt(input[i]) :
     sum += 0;
   }
   return sum;
